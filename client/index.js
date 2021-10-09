6bd753e18df5811b892ad5a110ca507338f4d269
invm <imichaelionov@gmail.com>
2021-10-09 19:32:04 +0300

    feat: add public facing http2 api

--- client/index.js
+++ client/index.js
@@ -1,19 +1,53 @@
-const assert = require('assert');
 require('dotenv').config('./.env');
-assert(process.env.SERVER_HOST, 'Error starting up, no environment variables');
+const assert = require('assert');
+const spdy = require('spdy');
+const express = require('express');
+const fs = require('fs');
+const app = express();
 const { establishConnection } = require('./connect');
 const { callCreateBlog, callDeleteBlog, callListBlogs, callReadBlog, callUpdateBlog } = require('./src/blog');
 
-const main = () => {
-	establishConnection();
-	// callListBlogs();
-	// callCreateBlog();
-	// callReadBlog();
-	// callUpdateBlog();
-	// callDeleteBlog();
-};
+assert(process.env.SERVER_HOST, 'Error starting up, no environment variables');
 
-main();
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+	callListBlogs(req, res);
+});
+
+app.get('/blog/:id', async (req, res) => {
+	callReadBlog(req, res);
+});
+
+app.post('/blog/', async (req, res) => {
+	callCreateBlog(req, res);
+});
+
+app.patch('/blog/:id', async (req, res) => {
+	callUpdateBlog(req, res);
+});
+
+app.delete('/blog/:id', async (req, res) => {
+	callDeleteBlog(req, res);
+});
+
+app.use((_, res) => {
+	res.status(404).send();
+});
+
+spdy.createServer(
+	{
+		key: fs.readFileSync('./public.key'),
+		cert: fs.readFileSync('./public.crt')
+	},
+	app
+).listen(8000, (err) => {
+	if (err) {
+		throw new Error(err);
+	}
+	establishConnection();
+	console.log('Listening on port 8000');
+});
 
 const handle = (signal) => {
 	console.log(`*^!@4=> Received event: ${signal}`);
